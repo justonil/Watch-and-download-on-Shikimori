b60ef7f4e2f9b9178e523d3c87bc37b9cbafbcd3
justonil <39393067+justonil@users.noreply.github.com>
2024-05-28 16:55:58 -0700

    Update manifest.user.js

--- manifest.user.js
+++ manifest.user.js
@@ -3,9 +3,9 @@
 // @namespace https://github.com/justonil/Watch-and-download-on-Shikimori
 // @description The addon adds the ability to watch and download anime on the Shikimori website.
 // @icon https://raw.github.com/justonil/Watch-and-download-on-Shikimori/main/images/icons/logo128.png
-// @author Malanavi
+// @author justonil
 // @homepageURL https://github.com/justonil/Watch-and-download-on-Shikimori
-// @version 3.0.5
+// @version 3.0.6
 // @copyright 2023, Malanavi
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 
